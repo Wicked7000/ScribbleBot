b98dcac23ca7103a65cbd61cd2cc5aebb64b1415
Jason Attwood <jason_attwood@hotmail.co.uk>
2020-07-13 04:40:43 +0100

    Add purge and retrieve commands

--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js');
+const { del } = require('request');
 const MongoClient = require('mongodb').MongoClient
 const client = new Discord.Client();
 
@@ -11,13 +12,45 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`)
 });
 
+collections = ['cah', 'skribbl']
+
+const fields = []
+collections.forEach((collection) => {
+    fields.push({ name: `${collection} add [TEXT_HERE]`, value: `Adds option to the ${collection} collection.`})
+    fields.push({ name: `${collection} retrieve`, value: `Retrives all options in the ${collection} collection.`})
+    fields.push({ name: `${collection} purge`, value: `Remove all options in the ${collection} collection.`})
+})
+
+function isHigherAccessLevel(message){
+    return message.author.tag === 'Wicked#3867' || message.author.tag === 'darko#0515';
+}
+
+async function purge(collection_name, message){
+    if(isHigherAccessLevel(message)){
+        const dbConnection = await dbClient.connect();
+        const wordsDatabase = dbConnection.db(databaseName)
+        const deleted = (await wordsDatabase.collection(collection_name).deleteMany(true)).result
+        if(deleted.ok){
+            message.reply(`All items have been purged from the collection! ${deleted.n}`)
+        } else {
+            message.reply('Error purging from collection!')
+        }        
+    }else{
+        message.reply('Permission not allowed, only Wicked or Darko can see collections.')
+    }
+}
+
 async function retrieveFromDatabase(collection_name, message){
-    if(message.author.tag === 'Wicked#3867' || message.author.tag === 'darko#0515'){
+    if(isHigherAccessLevel(message)){
         const dbConnection = await dbClient.connect();
         const wordsDatabase = dbConnection.db(databaseName)
         const allObjects = await wordsDatabase.collection(collection_name).find(true).toArray();
-        const stringifiedObjects = allObjects.map(item => item.content).join(', ')
-        message.reply(stringifiedObjects)
+        if(allObjects.length !== 0){
+            const stringifiedObjects = allObjects.map(item => item.content).join(', ')
+            message.reply(stringifiedObjects)
+        } else {
+            message.reply('No items are present within this collection!')
+        }        
     }else{
         message.reply('Permission not allowed, only Wicked or Darko can see collections.')
     }
@@ -29,7 +62,7 @@ async function sendToDatabase(author, content, collection_name, message){
     const matchingDocument = await wordsDatabase.collection(collection_name).findOne({ content });
     if(!matchingDocument){
         wordsDatabase.collection(collection_name).insertOne({
-            author,
+            author: author.tag,
             content,
         }, (error) => {
             if(error){
@@ -45,25 +78,23 @@ async function sendToDatabase(author, content, collection_name, message){
 
 client.on('message', async (message) => {
     if (!message.author.bot){
+        const msg_collection_name = message.content.split(" ")[0];
         if (message.content === 'help'){
             const helpEmbed = new Discord.MessageEmbed()
                 .setTitle('Help')
                 .setAuthor('Help - Commands', '', 'https://discord.js.org')
-                .addFields(
-                    { name: 'skribbl add [TEXT_HERE]', value: 'Adds to the database of skribbl words!' },
-                    { name: 'skribbl retrieve', value: 'Returns back all words currently in the skribbl database!' },
-                    { name: 'cah add [TEXT_HERE]', value: 'Adds to the database of cards against humanity words!' },
-                    { name: 'cah retrieve', value: 'Returns back all words currently in the cards against humanity database!' }
-                )
+                .addFields(fields)
             message.reply(helpEmbed)
-        }else if (message.content.match(/skribbl add.*/)){
-            sendToDatabase(message.author, message.content.split('skribbl add ')[1], 'skribbl', message)
-        }else if (message.content.match(/cah add.*/)){
-            sendToDatabase(message.author, message.content.split('cah add ')[1], 'cah', message)
-        }else if(message.content === 'skribbl retrieve'){
-            retrieveFromDatabase('skribbl', message)
-        }else if(message.content === 'cah retrieve'){
-            retrieveFromDatabase('cah', message)
+        }else if (collections.includes(msg_collection_name)){
+            const command = message.content.split(" ")[1]
+            if(command === 'add'){
+                const msg_content_option = message.content.split(" ")[2];
+                sendToDatabase(message.author, msg_content_option, msg_collection_name, message)
+            }else if(command === 'retrieve'){
+                retrieveFromDatabase(msg_collection_name, message);
+            }else if(command === 'purge'){
+                purge(msg_collection_name, message);
+            }
         }else{
             message.reply('That is not a valid command! see help')
         }
