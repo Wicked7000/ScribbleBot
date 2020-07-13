713ea6331b851ec8de201e3a2094a13190bfae68
Jason Attwood <jason_attwood@hotmail.co.uk>
2020-07-13 02:47:52 +0100

    Added push to mongodb

--- app/main.js
+++ app/main.js
@@ -1,5 +1,13 @@
 const Discord = require('discord.js')
+const MongoClient = require('mongodb').MongoClient
 const client = new Discord.Client();
+
+const connectionUrl = process.env.DB_URL
+const databaseName = 'words'
+
+const dbClient = new MongoClient(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@scribblecluster.sqash.mongodb.net/${databaseName}?retryWrites=true&w=majority`);
+const dbConnection = await dbClient.connect();
+const wordsDatabase = dbConnection.db(databaseName)
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`)
@@ -7,8 +15,20 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.channel.type === 'dm' && !message.author.bot){
-        message.reply('Added!')
+        message.reply('Attempting to add!')
+        wordsDatabase.collection('scribbleio').insertOne({
+            author: message.author.tag,
+            content: message.content
+        }, (error) => {
+            if(error){
+                message.reply('The message failed to add to the database please try again!');
+            }else{
+                message.reply('Message added!')
+            }
+        });
     }
 })
 
+
 client.login(process.env.BOT_TOKEN)
+
