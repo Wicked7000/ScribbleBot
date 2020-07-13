321de6dd289e341842d9cd170d113982b47557aa
Jason Attwood <jason_attwood@hotmail.co.uk>
2020-07-13 03:06:05 +0100

    Update bot operations

--- app/main.js
+++ app/main.js
@@ -11,24 +11,39 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`)
 });
 
+async function sendToDatabase(author, content, database_name, message){
+    const dbConnection = await dbClient.connect();
+    const wordsDatabase = dbConnection.db(databaseName)
+    wordsDatabase.collection(database_name).insertOne({
+        author,
+        content,
+    }, (error) => {
+        if(error){
+            message.reply('The message failed to add to the database please try again!');
+        }else{
+            message.reply('Message added!')
+        }
+    });
+}
+
 client.on('message', async (message) => {
-    if (message.channel.type === 'dm' && !message.author.bot){
-        message.reply('Attempting to add!')
-        
-        const dbConnection = await dbClient.connect();
-        const wordsDatabase = dbConnection.db(databaseName)
-        wordsDatabase.collection('scribbleio').insertOne({
-            author: message.author.tag,
-            content: message.content
-        }, (error) => {
-            if(error){
-                message.reply('The message failed to add to the database please try again!');
-            }else{
-                message.reply('Message added!')
-            }
-        });
+    if (message.content === 'help'){
+        const helpEmbed = new Discord.MessageEmbed()
+            .setTitle('Help')
+            .setAuthor('Help - Commands', '', 'https://discord.js.org')
+            .addFields(
+                { name: 'skribbl add [TEXT_HERE]', value: 'Adds to the database of skribbl words!' },
+                { name: 'cah add [TEXT_HERE]', value: 'Adds to the database of cards against humanity words!' }
+            )
+        message.reply(helpEmbed)
+    }else if (message.content.startsWith('skribbl add ')){
+        sendToDatabase(message.author, message.content, 'skribbl')
+    }else if (message.content.startsWith('cah add ')){
+        sendToDatabase(message.author, message.content, 'cah')
+    }else{
+        message.reply('That is not a valid command! see help')
     }
-})
+});
 
 
 client.login(process.env.BOT_TOKEN)
