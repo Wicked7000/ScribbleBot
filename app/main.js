78a7cafc6b3fe59af1ea8903a7a76b16ed80bc1b
Jason Attwood <jason_attwood@hotmail.co.uk>
2020-07-13 02:49:44 +0100

    Change the connection pattern

--- app/main.js
+++ app/main.js
@@ -6,16 +6,17 @@ const connectionUrl = process.env.DB_URL
 const databaseName = 'words'
 
 const dbClient = new MongoClient(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@scribblecluster.sqash.mongodb.net/${databaseName}?retryWrites=true&w=majority`);
-const dbConnection = await dbClient.connect();
-const wordsDatabase = dbConnection.db(databaseName)
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`)
 });
 
-client.on('message', message => {
+client.on('message', async (message) => {
     if (message.channel.type === 'dm' && !message.author.bot){
         message.reply('Attempting to add!')
+        
+        const dbConnection = dbClient.connect();
+        const wordsDatabase = dbConnection.db(databaseName)
         wordsDatabase.collection('scribbleio').insertOne({
             author: message.author.tag,
             content: message.content
